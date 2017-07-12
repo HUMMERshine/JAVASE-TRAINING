832120b44b940a0cbd96915e892ab8696040a5ad
lst-bytedance <a_lst@163.com>
2017-07-12 16:39:25 +0800

    7.12_second
    Change-Id: I75d1b65253a0b40dbf9c4d774596b102349e839f

--- src/priv/lst/thinkinjava/ClassLoaderTraining.java
+++ src/priv/lst/thinkinjava/ClassLoaderTraining.java
@@ -1,5 +1,75 @@
 package priv.lst.thinkinjava;
 
-public class ClassLoaderTraining {
-
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+/**
+ * 
+ * @author lst-bytedance
+ * @link http://www.importnew.com/23742.html
+ */
+public class ClassLoaderTraining extends ClassLoader{
+	public static void main(String[] args) {
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        System.out.println(loader);
+        System.out.println(loader.getParent());
+        System.out.println(loader.getParent().getParent());
+        
+        ClassLoaderTraining classLoader = new ClassLoaderTraining();
+        classLoader.setRoot("/Users/lst-bytedance/code");
+ 
+        Class<?> testClass = null;
+        try {
+            testClass = classLoader.loadClass("Hello");
+            Object object = testClass.newInstance();
+            System.out.println(object.getClass().getClassLoader());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        
+	}
+	
+	private String root;
+	 
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        byte[] classData = loadClassData(name);
+        if (classData == null) {
+            throw new ClassNotFoundException();
+        } else {
+            return defineClass(name, classData, 0, classData.length);
+        }
+    }
+ 
+    private byte[] loadClassData(String className) {
+        String fileName = root + File.separatorChar
+                + className.replace('.', File.separatorChar) + ".class";
+        try {
+            InputStream ins = new FileInputStream(fileName);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            int bufferSize = 1024;
+            byte[] buffer = new byte[bufferSize];
+            int length = 0;
+            while ((length = ins.read(buffer)) != -1) {
+                baos.write(buffer, 0, length);
+            }
+            return baos.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+ 
+    public String getRoot() {
+        return root;
+    }
+ 
+    public void setRoot(String root) {
+        this.root = root;
+    }
 }
